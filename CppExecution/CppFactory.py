a6be6209907a24809217706035b8080a6329f340
Piotr Lezanski <lezanskipiotr@gmail.com>
2023-06-04 11:59:33 +0200

    added cpp factory

---
+++ CppExecution/CppFactory.py
@@ -0,0 +1,26 @@
+import os
+from CppObject import *
+
+# TODO: add timeout
+
+class CppFactory:
+    def __init__(self, max_exec_time : int):
+        self.max_exec_time = max_exec_time
+    
+    # method receives C++ code as filepath and input as string
+    def CppObjectFromFilepath(self, file_path : str, input_text : str):
+        self.object = CppObject(file_path, input_text)
+        self.object.compile_and_run()
+
+    def exceededTime(self):
+        if self.object.get_execution_time() < self.max_exec_time:
+            return True
+        else:
+            return False
+
+    # method receives C++ code as string and input as string
+    def CppObjectFromString(self, code : str, input : str):
+        temp_file = open('tempCodeChecker.in', 'w+')
+        temp_file.write(code)
+        current_directory = os.path.abspath(os.getcwd())
+        object = CppObject(current_directory + "/tempCodeChecker.in", input)
