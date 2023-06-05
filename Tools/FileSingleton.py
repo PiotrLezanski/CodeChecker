990fb08271443eff36625d7e1e8819ef8aae52f3
xFloly <soszynski-f@o2.pl>
2023-06-05 19:21:56 +0200

    Udate change __file1 __file 2 to __file[] from now on all methods envoked with ID

--- Tools/FileSingleton.py
+++ Tools/FileSingleton.py
@@ -4,17 +4,16 @@ from typing import Optional, IO
 
 class FileSingleton(object):
     __instance = None
-    __file1: Optional[IO] = None  # file1 is the left file
-    __file2: Optional[IO] = None  # file2 is the right file
-    __filepath1: Optional[str] = None
-    __filepath2: Optional[str] = None
+    __file = [Optional[IO], Optional[IO]]
+    __filepath = [Optional[str], Optional[str]]
+    __default = 0
 
     def __init__(self):
         if FileSingleton.__instance is not None:
             raise Exception("Direct initialization of singleton is not allowed. Use get_instance() instead.")
         else:
             FileSingleton.__instance = self
-
+            
     @staticmethod
     def get_instance():
         if FileSingleton.__instance is None:
@@ -22,92 +21,56 @@ class FileSingleton(object):
         return FileSingleton.__instance
 
     @staticmethod
-    def get_file1():
-        return FileSingleton.__file1
+    def set_default(i):
+        FileSingleton.__default = i
 
     @staticmethod
-    def get_file2():
-        return FileSingleton.__file2
+    def get_file(i=__default):
+        return FileSingleton.__file[i]
 
     @staticmethod
-    def get_file1_text():
-        return FileSingleton.__file1.read()
+    def get_file_text(i=__default):
+        return FileSingleton.__file[i].read()
 
     @staticmethod
-    def get_file2_text():
-        return FileSingleton.__file2.read()
-
-    @staticmethod
-    def get_filepath1():
-        return FileSingleton.__filepath1
-
-    @staticmethod
-    def get_filepath2():
-        return FileSingleton.__filepath2
+    def get_filepath(i=__default):
+        return FileSingleton.__filepath[i]
 
     # before reading new file close old file
     # if wrong extension, raise exception WrongExtensionError
     # if you cant use WrongExtensionError, type from Tools.Exceptions import WrongExtensionError
     # if wrong path, raise exception FileNotFoundError
     @staticmethod
-    def set_file1(file_path):
+    def set_file(file_path, i=__default):
         if file_path.endswith(".cpp") or file_path.endswith(".h"):
             try:
-                FileSingleton.__close_file1()
+                FileSingleton.__close_file(i)
 
                 # I'm not sure if I should check if file1 and file2 are the same
 
-                # if FileSingleton.__filepath2 == file_path:
+                # if FileSingleton.__filepath[k] == file_path:
                 #     raise SameFilesError("File1 and file2 cannot be the same")
 
-                FileSingleton.__file1 = open(file_path, 'r')
-                FileSingleton.__filepath1 = file_path
-            except Exception as e:
-                raise e
-        else:
-            raise WrongExtensionError("File must be a .cpp or .h file")
-
-    # before reading new file close old file if and only if new path is correct
-    # if wrong extension, raise exception WrongExtensionError
-    # if you cant use WrongExtensionError, type from Tools.Exceptions import WrongExtensionError
-    # if wrong path, raise exception FileNotFoundError
-    @staticmethod
-    def set_file2(file_path):
-        if file_path.endswith(".cpp") or file_path.endswith(".h"):
-            try:
-                FileSingleton.__close_file2()
-
-                # I'm not sure if I should check if file1 and file2 are the same
-
-                # if FileSingleton.__filepath1 == file_path:
-                #     raise SameFilesError("File1 and file2 cannot be the same")
-
-                FileSingleton.__file2 = open(file_path, 'r')
-                FileSingleton.__filepath2 = file_path
+                FileSingleton.__file[i] = open(file_path, 'r')
+                FileSingleton.__filepath[i] = file_path
             except Exception as e:
                 raise e
         else:
             raise WrongExtensionError("File must be a .cpp or .h file")
 
     @staticmethod
-    def __close_file1():
-        if FileSingleton.__file1 is not None:
-            FileSingleton.__file1.close()
+    def __close_file(i=__default):
+        if FileSingleton.__file[i] is not None:
+            FileSingleton.__file[i].close()
             FileSingleton.__path1 = None
 
     @staticmethod
-    def __close_file2():
-        if FileSingleton.__file2 is not None:
-            FileSingleton.__file2.close()
-            FileSingleton.__path2 = None
-
-    @staticmethod
     def reset():
-        if FileSingleton.__file1 is not None:
-            FileSingleton.__close_file1()
-        if FileSingleton.__file2 is not None:
-            FileSingleton.__close_file2()
-        FileSingleton.__file1 = None
-        FileSingleton.__file2 = None
-        FileSingleton.__filepath1 = None
-        FileSingleton.__filepath2 = None
+        if FileSingleton.__file[0] is not None:
+            FileSingleton.__close_file(0)
+        if FileSingleton.__file[1] is not None:
+            FileSingleton.__close_file(1)
+        FileSingleton.__file[0] = None
+        FileSingleton.__file[1] = None
+        FileSingleton.__filepath[0] = None
+        FileSingleton.__filepath[1] = None
