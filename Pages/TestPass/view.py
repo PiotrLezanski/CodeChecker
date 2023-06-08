05bccdda7b5b5bef676e5c2dd8b2bee0ce9223d3
Piotr Lezanski <lezanskipiotr@gmail.com>
2023-06-08 17:53:39 +0200

    added scrollable frame

--- Pages/TestPass/view.py
+++ Pages/TestPass/view.py
@@ -5,6 +5,7 @@ from Pages.TestPass.controller import Controller
 class View(ctk.CTkFrame):
     def __init__(self, parent):
         ctk.CTkFrame.__init__(self, parent)
+        self.testcases_frame = None
         self.run_all_button = None
         self.run_test_buttons = None
         self.output_texts = None
@@ -44,7 +45,7 @@ class View(ctk.CTkFrame):
 
     def create_testcase_components(self):
         # create RUN ALL button
-        self.run_all_button = ctk.CTkButton(self, text="RUN ALL TESTS", command=self.controller.run_all_testcases)
+        self.run_all_button = ctk.CTkButton(self, text="RUN ALL TESTS", command=self.controller.run_all_testcases, width=300)
         self.run_all_button.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
 
         self.testcase_frames = [None] * int(self.number_of_tests)
@@ -55,8 +56,10 @@ class View(ctk.CTkFrame):
         self.output_texts = [None] * int(self.number_of_tests)
         self.run_test_buttons = [None] * int(self.number_of_tests)
 
+        self.testcases_frame = ctk.CTkScrollableFrame(self, width=600, height=450)
+        self.testcases_frame.grid(row=3, column=0)
         for i in range(int(self.number_of_tests)):
-            self.testcase_frames[i] = ctk.CTkFrame(self, border_width=2)
+            self.testcase_frames[i] = ctk.CTkFrame(self.testcases_frame, border_width=2)
             self.input_labels[i] = ctk.CTkLabel(self.testcase_frames[i], text="Input")
             self.input_texts[i] = ctk.CTkTextbox(self.testcase_frames[i], height=60, width=460)
             self.separators[i] = tkinter.ttk.Separator(self.testcase_frames[i], orient="horizontal", style='TSeparator')
